8cb5603546fa389421a2dca98a55ecf18abb020e
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-09-11 16:10:55 +1200

    and test that the relayhost port can also be set

--- spec/classes/postfix_spec.rb
+++ spec/classes/postfix_spec.rb
@@ -15,11 +15,13 @@ describe 'postfix', :type => :class do
       'path'    => '/etc/postfix/main.cf'
     ) }
     it { should contain_augeas('postfix_config') }
+      
     describe 'working with the default main.cf' do
       describe_augeas 'postfix_config', :target => 'etc/postfix/main.cf' do
         it { should_not execute.with_change }
       end
     end
+    
     describe 'working with a modified main.cf' do
       describe_augeas 'postfix_config', :lens => 'Postfix_Main.lns', :target => 'etc/postfix/main.cf', :fixture => 'etc/postfix/main.modified.cf' do
         it { should execute.with_change }
@@ -32,6 +34,7 @@ describe 'postfix', :type => :class do
         it { should execute.idempotently }
       end
     end
+    
     describe 'with myorigin => example.org and the default main.cf' do
       let :params do
         { :myorigin => 'example.org' }
@@ -44,6 +47,7 @@ describe 'postfix', :type => :class do
         it { should execute.idempotently }
       end
     end
+    
     describe 'with myorigin => example.org and a modifies main.cf' do
       let :params do
         { :myorigin => 'example.org' }
@@ -56,6 +60,7 @@ describe 'postfix', :type => :class do
         it { should execute.idempotently }
       end
     end
+    
     describe 'with relayhost => smtp.example.org and the default main.cf' do
       let :params do
         { :relayhost => 'smtp.example.org' }
@@ -68,6 +73,7 @@ describe 'postfix', :type => :class do
         it { should execute.idempotently }
       end
     end
+    
     describe 'with relayhost => smtp.example.org and a modified main.cf' do
       let :params do
         { :relayhost => 'smtp.example.org' }
@@ -80,6 +86,37 @@ describe 'postfix', :type => :class do
         it { should execute.idempotently }
       end
     end
+    
+    describe 'with relayhost => smtp.example.org, relayhost_port => 42 and the default main.cf' do
+          let :params do
+            { :relayhost      => 'smtp.example.org',
+              :relayhost_port => '42',
+            }
+          end
+          describe_augeas 'postfix_config', :lens => 'Postfix_Main.lns', :target => 'etc/postfix/main.cf' do
+            it { should execute.with_change }
+            it 'should match relayhost' do
+              aug_get('relayhost').should == 'smtp.example.org:42'
+            end
+            it { should execute.idempotently }
+          end
+        end
+        
+        describe 'with relayhost => smtp.example.org, relayhost_port => 42 and a modified main.cf' do
+          let :params do
+            { :relayhost      => 'smtp.example.org',
+              :relayhost_port => '42',
+            }
+          end
+          describe_augeas 'postfix_config', :lens => 'Postfix_Main.lns', :target => 'etc/postfix/main.cf', :fixture => 'etc/postfix/main.modified.cf' do
+            it { should execute.with_change }
+            it 'should match relayhost' do
+              aug_get('relayhost').should == 'smtp.example.org:42'
+            end
+            it { should execute.idempotently }
+          end
+        end
+    
     describe 'with remove_sendmail => true' do
       let :params do
         { :remove_sendmail => true }
@@ -87,7 +124,9 @@ describe 'postfix', :type => :class do
       it { should contain_service('sendmail').with_ensure('stopped') }
       it { should contain_package('sendmail').with_ensure('purged') }
     end
+    
   end
+  
   context 'on a RedHat OS' do
       let :facts do
         {
@@ -102,11 +141,13 @@ describe 'postfix', :type => :class do
       'path'    => '/etc/postfix/main.cf'
     ) }
     it { should contain_augeas('postfix_config') }
+      
     describe 'working with the default main.cf' do
       describe_augeas 'postfix_config', :target => 'etc/postfix/main.cf' do
         it { should_not execute.with_change }
       end
     end
+    
     describe 'working with a modified main.cf' do
       describe_augeas 'postfix_config', :lens => 'Postfix_Main.lns', :target => 'etc/postfix/main.cf', :fixture => 'etc/postfix/main.modified.cf' do
         it { should execute.with_change }
@@ -119,6 +160,7 @@ describe 'postfix', :type => :class do
         it { should execute.idempotently }
       end
     end
+    
     describe 'with myorigin => example.org and the default main.cf' do
       let :params do
         { :myorigin => 'example.org' }
@@ -131,6 +173,7 @@ describe 'postfix', :type => :class do
         it { should execute.idempotently }
       end
     end
+    
     describe 'with myorigin => example.org and a modifies main.cf' do
       let :params do
         { :myorigin => 'example.org' }
@@ -143,6 +186,7 @@ describe 'postfix', :type => :class do
         it { should execute.idempotently }
       end
     end
+    
     describe 'with relayhost => smtp.example.org and the default main.cf' do
       let :params do
         { :relayhost => 'smtp.example.org' }
@@ -155,6 +199,7 @@ describe 'postfix', :type => :class do
         it { should execute.idempotently }
       end
     end
+    
     describe 'with relayhost => smtp.example.org and a modified main.cf' do
       let :params do
         { :relayhost => 'smtp.example.org' }
@@ -167,6 +212,37 @@ describe 'postfix', :type => :class do
         it { should execute.idempotently }
       end
     end
+    
+    describe 'with relayhost => smtp.example.org, relayhost_port => 42 and the default main.cf' do
+      let :params do
+        { :relayhost      => 'smtp.example.org',
+          :relayhost_port => '42',
+        }
+      end
+      describe_augeas 'postfix_config', :lens => 'Postfix_Main.lns', :target => 'etc/postfix/main.cf' do
+        it { should execute.with_change }
+        it 'should match relayhost' do
+          aug_get('relayhost').should == 'smtp.example.org:42'
+        end
+        it { should execute.idempotently }
+      end
+    end
+    
+    describe 'with relayhost => smtp.example.org, relayhost_port => 42 and a modified main.cf' do
+      let :params do
+        { :relayhost      => 'smtp.example.org',
+          :relayhost_port => '42',
+        }
+      end
+      describe_augeas 'postfix_config', :lens => 'Postfix_Main.lns', :target => 'etc/postfix/main.cf', :fixture => 'etc/postfix/main.modified.cf' do
+        it { should execute.with_change }
+        it 'should match relayhost' do
+          aug_get('relayhost').should == 'smtp.example.org:42'
+        end
+        it { should execute.idempotently }
+      end
+    end
+    
     describe 'with remove_sendmail => true' do
       let :params do
         { :remove_sendmail => true }
@@ -174,5 +250,7 @@ describe 'postfix', :type => :class do
       it { should contain_service('sendmail').with_ensure('stopped') }
       it { should contain_package('sendmail').with_ensure('absent') }
     end
+    
   end
+  
 end
